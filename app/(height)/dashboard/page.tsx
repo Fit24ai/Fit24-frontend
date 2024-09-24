b490871123409c397d5c65c390cb365dedeb6f05
YashT1005 <yashthakur10052@gmail.com>
2024-09-24 16:30:14 +0530

    chamges

--- app/(height)/dashboard/page.tsx
+++ app/(height)/dashboard/page.tsx
@@ -7,7 +7,7 @@ import { ReferralDialog } from "@/components/shared/ReferralDialog"
 import { useWallet } from "@/hooks/useWallet"
 import React from "react"
 
-export default function page({
+export default function DashboardPage({
   searchParams,
 }: {
   searchParams: { stakeRef: string | undefined }
