e318474771ab46bb11cf96c8cb1a6c1e73688df7
Yash Thakur <yashthakur10052@gmail.com>
2025-02-05 17:55:23 +0530

    google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import Web3ModalProvider from "@/providers/Web3ModalProvider"
 import { WalletProvider } from "@/hooks/useWallet"
 import { ParamsProvider } from "@/context/useParams"
 import { headers } from "next/headers"
+import Script from "next/script"
 
 // Importing the Readex Pro font
 const readexPro = Readex_Pro({ subsets: ["latin"] })
@@ -23,6 +24,23 @@ export default function RootLayout({
   return (
     <ParamsProvider>
       <html lang="en">
+        <head>
+          <Script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-7MRHGSSDRP"
+          ></Script>
+          <Script
+            id={"google-tag-manager-script"}
+            dangerouslySetInnerHTML={{
+              __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-7MRHGSSDRP');
+              `,
+            }}
+          />
+        </head>
         <body className={`${readexPro.className}   max-w-[1730px] mx-auto`}>
           <Web3ModalProvider cookies={cookies}>
             <WalletProvider>
