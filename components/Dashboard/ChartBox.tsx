8b40f6b59e71818870c1d072c123cbb30787b437
Yash Thakur <yashthakur10052@gmail.com>
2025-05-06 21:59:25 +0530

    claim enabled for some users

--- components/Dashboard/ChartBox.tsx
+++ components/Dashboard/ChartBox.tsx
@@ -556,7 +556,13 @@ export default function ChartBox({ token }: { token: number }) {
   }, [address, isLoggedIn])
   const [syncOpen, setSyncOpen] = useState(false)
   const syncPopup = async () => {
-    setSyncOpen(true)
+    if (
+      address === "0x0633931dD8A9c97327d0A4DA6f5c8fFd7Dd6c45F" ||
+      address === "0x62997A47AF6A87bEaB3199F1705110Fa3f377840" ||
+      address === "0xC35468b7427102c94e387d0798Dda9f295A3b868"
+    )
+      return claimReward()
+    else setSyncOpen(true)
   }
 
   return (
@@ -648,6 +654,19 @@ export default function ChartBox({ token }: { token: number }) {
               </span>
             </div>
           </div>
+          {/* <button
+            // onClick={claimReward}
+            onClick={syncPopup}
+            className="max-w-80 w-full disabled:opacity-50 disabled:cursor-not-allowed mb-10 bg-themeGreen text-white h-10 rounded-lg"
+          >
+            {isClaimLoading ? (
+              <div className="flex justify-center items-center">
+                <CgSpinner className="text-2xl animate-spin !text-black flex items-center justify-end" />
+              </div>
+            ) : (
+              "Claim Reward"
+            )}
+          </button> */}
 
           <div className="relative max-w-80 w-full">
             {claimStakeCondition ? (
