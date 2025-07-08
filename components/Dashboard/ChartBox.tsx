1095f5a759c95dedce56d38d39e5a94a7cc801a0
Yash Thakur <yashthakur10052@gmail.com>
2025-07-08 23:09:49 +0530

    changes

--- components/Dashboard/ChartBox.tsx
+++ components/Dashboard/ChartBox.tsx
@@ -1160,7 +1160,7 @@ export default function ChartBox({ token }: { token: number }) {
             {claimStakeCondition ? (
               <div className="text-center">
                 {/* Available to claim after 24 Hours */}
-                Available to claim on 8th July
+                Available to claim on 10th July
               </div>
             ) : (
               <button
